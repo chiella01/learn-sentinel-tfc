d3f2dc5e1f94ea7fdba5e749ef3066a077fbc4cd
chiella01 <85824895+chiella01@users.noreply.github.com>
2023-03-31 04:51:04 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "1.4.4"
+      version = "3.0.1"
     }
     aws = {
       source  = "hashicorp/aws"
