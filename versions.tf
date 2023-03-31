303399744dfc15bf02301fc524bd01d8aa43d48e
chiella01 <85824895+chiella01@users.noreply.github.com>
2023-03-31 03:32:03 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<terraform-chiella>"
+    organization = "chiella"
 
     workspaces {
       name = "sentinel-example"
